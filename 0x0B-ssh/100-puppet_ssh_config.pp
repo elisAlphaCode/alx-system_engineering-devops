ae0faa06692399f50b7d63d733648aa9c1a47b6e
elisAlphaCode <elis.joshua55@gmail.com>
2022-09-21 05:58:50 +0100

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,4 @@
+# puppet ssh configuration
+exec { 'ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
